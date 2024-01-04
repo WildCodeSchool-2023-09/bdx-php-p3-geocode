c18f2ae4cbd766d4d40fb3c969a568e28207c2e4
Nicolas Leroux <hertford.fr@gmail.com>
2024-01-04 15:23:43 +0100

    change home route

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MapController extends AbstractController
 {
-    #[Route('/map', name: 'app_map')]
+    #[Route('/', name: 'app_map')]
     public function index(): Response
     {
         return $this->render('map/index.html.twig', [
