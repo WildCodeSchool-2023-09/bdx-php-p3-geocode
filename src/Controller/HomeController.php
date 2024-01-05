4bb062ddf5b090185de5f3582fc0f0f06ba33da8
Nicolas Leroux <hertford.fr@gmail.com>
2024-01-05 11:23:01 +0100

    redirect app_home to app_map

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,9 +8,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-//    #[Route('/', name: 'app_home')]
-//    public function index(): Response
-//    {
-//        return $this->render('home/index.html.twig');
-//    }
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->redirect('app_map');
+    }
 }
