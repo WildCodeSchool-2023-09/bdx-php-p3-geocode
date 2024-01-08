f4a92dbadfbb4d6e10c000a280d191ce436a3a0b
Nicolas Leroux <hertford.fr@gmail.com>
2024-01-08 12:35:32 +0100

    add town fixtures

--- src/DataFixtures/TownFixtures.php
+++ src/DataFixtures/TownFixtures.php
@@ -2,15 +2,18 @@
 
 namespace App\DataFixtures;
 
+use App\Service\CsvService;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class TownFixtures extends Fixture
 {
+    public function __construct(private CsvService $csvService)
+    {
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-        $manager->flush();
+        $this->csvService->readTown();
     }
 }
