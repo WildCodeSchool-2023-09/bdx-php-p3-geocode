2d786b9445a87afe2455ab2d464772b73a62058c
Nicolas Leroux <hertford.fr@gmail.com>
2024-01-09 19:26:04 +0100

    add a class to prepare town data

---
+++ src/Service/PrepareTownService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Service;
+
+class PrepareTownService
+{
+    public function prepareTownName(string $name): string
+    {
+        $string = strtoupper($name);
+        return str_replace([' L ', ' S ', ' D '], [' L\'', ' S\'', ' D\''], $string);
+    }
+
+    public function preparePos(string $pos): float
+    {
+        return round(floatval($pos), 4);
+    }
+
+    public function prepareZipCode(string $zipCode): string
+    {
+        if (!preg_match('/\d{5}/', $zipCode)) {
+            $zipCode = '0' . $zipCode;
+        }
+        return $zipCode;
+    }
+}
