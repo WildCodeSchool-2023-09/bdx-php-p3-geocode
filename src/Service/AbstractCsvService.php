8d374b556fda3ff6b25a821b3f739e3fce46f927
Nicolas Leroux <hertford.fr@gmail.com>
2024-01-11 19:42:36 +0100

    construct and Filename methods for abstractCsvService

--- src/Service/AbstractCsvService.php
+++ src/Service/AbstractCsvService.php
@@ -2,6 +2,40 @@
 
 namespace App\Service;
 
-class AbstractCsvService
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+
+abstract class AbstractCsvService
 {
+    protected mixed $file;
+
+    /**
+     * @throws Exception
+     */
+    public function __construct(
+        protected string $filename,
+        protected EntityManagerInterface $entityManager,
+        protected PrepareTown $prepareTown = new PrepareTown()
+    ) {
+        $this->verifyFilename();
+        //on ouvre le fichier en mode lecture
+        $this->file = fopen($this->getFilename(), "r");
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function verifyFilename(): void
+    {
+        if (!is_file($this->getFilename())) {
+            fclose($this->file);
+            throw new Exception('File not found.' . PHP_EOL .
+                'Verify your sources directory or yours parameters in config.yalm');
+        }
+    }
+
+    public function getFilename(): string
+    {
+        return dirname('.', 2) . $this->filename;
+    }
 }
