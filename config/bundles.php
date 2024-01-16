050601ecca5d1611a99e898af35aaf9107b770d6
Nicolas Leroux <hertford.fr@gmail.com>
2024-01-16 08:17:58 +0100

    composer require symfony/ux-twig-component it seems everything is ok

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
 ];
