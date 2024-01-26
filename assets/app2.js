68c8b9fc70ea8130e7ccad737d3bcabee937227a
Nicolas Leroux <hertford.fr@gmail.com>
2024-01-26 14:53:02 +0100

    for test

---
+++ assets/app2.js
@@ -0,0 +1,3 @@
+import './styles/app2.scss';
+
+
--- src/Controller/RouteController.php
+++ src/Controller/RouteController.php
@@ -11,8 +11,24 @@ class RouteController extends AbstractController
     #[Route('/route', name: 'app_route')]
     public function index(): Response
     {
-        return $this->render('route/index.html.twig', [
-            'controller_name' => 'RouteController',
-        ]);
+        return $this->render('route/index.html.twig');
+    }
+
+    #[Route('/route2', name: 'app_route2')]
+    public function index2(): Response
+    {
+        return $this->render('route/index2.html.twig');
+    }
+
+    #[Route('/route5', name: 'app_route5')]
+    public function index5(): Response
+    {
+        return $this->render('route/index5.html.twig');
+    }
+
+    #[Route('/route4', name: 'app_route4')]
+    public function index4(): Response
+    {
+        return $this->render('route/index4.html.twig');
     }
 }
