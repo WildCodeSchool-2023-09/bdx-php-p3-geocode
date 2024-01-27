6f334a2917a5ea905e33f48cd7a25e175390b5bf
Didixe <Adeline.gilet@gmail.com>
2024-01-27 16:30:51 +0100

    fixtures for user and car

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
@@ -19,8 +20,9 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
+
         $user = new User();
-        $user->setGender('Autre');
+        $user->setGender('Ne pas spécifier');
         $user->setFirstname('Toto');
         $user->setLastname('Toto');
         $user->setBirthday(DateTime::createFromFormat('d/m/Y', '12/12/2002'));
@@ -32,6 +34,26 @@ class UserFixtures extends Fixture
         );
         $user->setPassword($hashedPassword);
         $manager->persist($user);
+        $manager->flush();
+
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setGender($faker->randomElement(['Homme', 'Femme', 'Non binaire', 'Ne pas spécifier']));
+            $user->setFirstname($faker->firstName);
+            $user->setLastname($faker->lastName);
+            $user->setBirthday($faker->dateTimeBetween('-80 years', '-18 years'));
+            $user->setEmail($faker->email);
+            $user->setRoles(['ROLE_CONTRIBUTOR']);
+            $hashedPassword = $this->passwordHasher->hashPassword(
+                $user,
+                'azertyui'
+            );
+            $user->setPassword($hashedPassword);
+            $manager->persist($user);
+        }
+
         $manager->flush();
     }
 }
