6fec5d14f750af80c734f0b213dc5b3116aea0a0
Didixe <Adeline.gilet@gmail.com>
2024-02-05 11:46:02 +0100

    fix login

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -56,7 +56,7 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
 //        }
 
         // For example:
-         return new RedirectResponse($this->urlGenerator->generate('app_home'));
+         return new RedirectResponse($this->urlGenerator->generate('app_login'));
 //        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
