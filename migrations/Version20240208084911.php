1be07f86606de4a894a080074c89fe08923d302d
Didixe <Adeline.gilet@gmail.com>
2024-02-08 09:53:33 +0100

    migration

--- migrations/Version20240208084911.php
+++ migrations/Version20240208084911.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240206133450 extends AbstractMigration
+final class Version20240208084911 extends AbstractMigration
 {
     public function getDescription(): string
     {
